4f03323ce37497f1bfbf1b999f6944bb32c6f2a1
monoking0401 <monoking0401@gmail.com>
2026-08-27 06:02:09 +0800

    Switch audited branch entrypoint to V3

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from v2 import main_v2 as _v2
-from v2.flower_engine_pt_south import calculate
+from v2.flower_engine_v3 import calculate, format_result
 
 _v2.calculate = calculate
+_v2.format_result = format_result
 app = _v2.app
