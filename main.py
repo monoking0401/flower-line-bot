59e3c130ee8cbde80be719820f61ab84e5a8c9ea
monoking0401 <monoking0401@gmail.com>
2026-08-29 14:40:41 +0800

    Route production through dispatch guard

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from v2 import main_v2 as _v2
-from v2.flower_engine_production import calculate, format_result
+from v2.flower_engine_dispatch_guard import calculate, format_result
 
 _v2.calculate = calculate
 _v2.format_result = format_result
