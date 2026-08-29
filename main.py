a702f437c4a550b850c2b37b9d7b3e9fca300d33
monoking0401 <monoking0401@gmail.com>
2026-08-29 14:27:52 +0800

    Use production formatter for settlement verification output

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from v2 import main_v2 as _v2
-from v2.flower_engine_production import calculate
+from v2.flower_engine_production import calculate, format_result
 
 _v2.calculate = calculate
+_v2.format_result = format_result
 app = _v2.app
